243d6451529983e8556b550a31375aebfa36c8f9
Dhananjay-230 <dhananjay.k@ahduni.edu.in>
2025-07-17 23:11:51 +0530

    new changes

--- DataWarehouse/scripts/bronze/load_bronze.sql
+++ DataWarehouse/scripts/bronze/load_bronze.sql
@@ -28,7 +28,7 @@ BEGIN
 
         PRINT '>> Inserting Data Into: bronze.crm_cust_info';
         BULK INSERT bronze.crm_cust_info
-        FROM 'C:\Enterprise Retail Data Warehouse\datasets\source_crm\cust_info.csv'
+        FROM 'C:\Enterprise-Retail-Data-Warehouse-and-Analytics\DataWarehouse\datasets\source_crm\cust_info.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -41,7 +41,7 @@ BEGIN
 
         PRINT '>> Inserting Data Into: bronze.crm_prd_info';
         BULK INSERT bronze.crm_prd_info
-        FROM 'C:\Enterprise Retail Data Warehouse\datasets\source_crm\prd_info.csv'
+        FROM 'C:\Enterprise-Retail-Data-Warehouse-and-Analytics\DataWarehouse\datasets\source_crm\prd_info.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -54,7 +54,7 @@ BEGIN
 
         PRINT '>> Inserting Data Into: bronze.crm_sales_details';
         BULK INSERT bronze.crm_sales_details
-        FROM 'C:\Enterprise Retail Data Warehouse\datasets\source_crm\sales_details.csv'
+        FROM 'C:\Enterprise-Retail-Data-Warehouse-and-Analytics\DataWarehouse\datasets\source_crm\sales_details.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -74,7 +74,7 @@ BEGIN
 
         PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
         BULK INSERT bronze.erp_loc_a101
-        FROM 'C:\Enterprise Retail Data Warehouse\datasets\source_erp\LOC_A101.csv'
+        FROM 'C:\Enterprise-Retail-Data-Warehouse-and-Analytics\DataWarehouse\datasets\source_erp\LOC_A101.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -87,7 +87,7 @@ BEGIN
 
         PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
         BULK INSERT bronze.erp_cust_az12
-        FROM 'C:\Enterprise Retail Data Warehouse\datasets\source_erp\CUST_AZ12.csv'
+        FROM 'C:\Enterprise-Retail-Data-Warehouse-and-Analytics\DataWarehouse\datasets\source_erp\CUST_AZ12.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
@@ -100,7 +100,7 @@ BEGIN
 
         PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
         BULK INSERT bronze.erp_px_cat_g1v2
-        FROM 'C:\Enterprise Retail Data Warehouse\datasets\source_erp\PX_CAT_G1V2.csv'
+        FROM 'C:\Enterprise-Retail-Data-Warehouse-and-Analytics\DataWarehouse\datasets\source_erp\PX_CAT_G1V2.csv'
         WITH (
             FIRSTROW = 2,
             FIELDTERMINATOR = ',',
