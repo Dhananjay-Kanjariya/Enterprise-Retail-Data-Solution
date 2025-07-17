7ac82a289532c32a020b202b626785f90f23fb29
Dhananjay Kanjariya <dhananjaykanjariya69@outlook.com>
2025-07-17 19:46:06 +0530

    Update ddl_silver_init.sql

--- scripts/silver/ddl_silver_init.sql
+++ scripts/silver/ddl_silver_init.sql
@@ -2,6 +2,7 @@
     This script creates curated (silver layer) tables in the 'silver' schema
     of the DataWarehouse database. These tables store cleaned and transformed data
     sourced from the raw bronze layer (CRM and ERP systems).
+    Few columns are added to the tables after identifing the transformation required.
 */
 
 USE DataWarehouse;
@@ -30,14 +31,15 @@ IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
 GO
 
 CREATE TABLE silver.crm_prd_info (
-    prd_id       INT,
-    prd_key      NVARCHAR(50),
-    prd_nm       NVARCHAR(50),
-    prd_cost     INT,
-    prd_line     NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt   DATETIME,
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
+    prd_id          INT,
+    cat_id          NVARCHAR(50),
+    prd_key         NVARCHAR(50),
+    prd_nm          NVARCHAR(50),
+    prd_cost        INT,
+    prd_line        NVARCHAR(50),
+    prd_start_dt    DATE,
+    prd_end_dt      DATE,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -47,16 +49,16 @@ IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
 GO
 
 CREATE TABLE silver.crm_sales_details (
-    sls_ord_num  NVARCHAR(50),
-    sls_prd_key  NVARCHAR(50),
-    sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
-    sls_sales    INT,
-    sls_quantity INT,
-    sls_price    INT,
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
+    sls_ord_num     NVARCHAR(50),
+    sls_prd_key     NVARCHAR(50),
+    sls_cust_id     INT,
+    sls_order_dt    DATE,
+    sls_ship_dt     DATE,
+    sls_due_dt      DATE,
+    sls_sales       INT,
+    sls_quantity    INT,
+    sls_price       INT,
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -68,7 +70,7 @@ GO
 CREATE TABLE silver.erp_loc_a101 (
     cid    NVARCHAR(50),
     cntry  NVARCHAR(50),
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -81,7 +83,7 @@ CREATE TABLE silver.erp_cust_az12 (
     cid    NVARCHAR(50),
     bdate  DATE,
     gen    NVARCHAR(50),
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
 
@@ -95,6 +97,6 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     cat          NVARCHAR(50),
     subcat       NVARCHAR(50),
     maintenance  NVARCHAR(50),
-	dwh_create_date DATETIME2 DEFAULT GETDATE()
+    dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
